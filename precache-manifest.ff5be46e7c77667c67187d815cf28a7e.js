d49b084ad4ea04f1c2260e03271bc93299ac710e
Hayley Tong <hayleytong@gmail.com>
2020-08-06 22:40:18 +0100

    Updates

---
+++ precache-manifest.ff5be46e7c77667c67187d815cf28a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0869c824426b945dcdd2340350132fe3",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "db76fa9ea1d25f72be30",
+    "url": "/weather-app/static/css/main.3604ea0e.chunk.css"
+  },
+  {
+    "revision": "90e45767320fccf39c48",
+    "url": "/weather-app/static/js/2.4350f6ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.4350f6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db76fa9ea1d25f72be30",
+    "url": "/weather-app/static/js/main.da504e2d.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
